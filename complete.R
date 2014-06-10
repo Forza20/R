ae69d327b5871a9c2fd05df4a5f470c67f3db084
Peter Lindholm-Bekker <Forza20@hotmail.com>
2014-06-10 20:05:21 +0200

    Create complete.R

---
+++ complete.R
@@ -0,0 +1,16 @@
+complete <- function(directory, id = 1:332) { 
+
+nobsNum <- numeric(0)
+
+    for (cid in id) {
+        cDfr <- pollutantmean(cid, directory)
+        nobsNum <- c(nobsNum, nrow(na.omit(cDfr)))}
+    data.frame(id = id, nobs = nobsNum)}
+
+pollutantmean <- function(id, directory, summarize = FALSE) {  fileStr <- paste(directory, "/", sprintf("%03d", as.numeric(id)), ".csv", 
+        sep = "")
+rawDfr <- read.csv(fileStr)
+  if (summarize) {
+        print(summary(rawDfr))}
+        
+return(rawDfr)}
