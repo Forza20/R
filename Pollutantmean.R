3a5871562c1bdbb1127d5e311e60a9511ba29060
Peter Lindholm-Bekker <Forza20@hotmail.com>
2014-06-10 20:08:27 +0200

    Update Pollutantmean.R

--- Pollutantmean.R
+++ Pollutantmean.R
@@ -8,5 +8,6 @@ for (i in id) {
 newRead = read.csv(paste(directory, "/", formatC(i, width = 3, flag = "0"), ".csv", sep = ""))                              data = c(data, newRead[[pollutant]])}
 return(mean(data, na.rm = TRUE))}
 
+#test
 > pollutantmean("specdata", "nitrate", 23)
 [1] 1.280833
